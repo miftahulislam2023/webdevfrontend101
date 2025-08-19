aa5f788a2b6dee06398f3887a2485fd2a44a93c9
Miftahul Islam <138649177+miftahulislam2023@users.noreply.github.com>
2025-08-19 22:20:39 +0600

    Add: [Javascript] Class 1 - Running JS, Comments, alert, console log

---
+++ Javascript/class-1/script.js
@@ -0,0 +1,15 @@
+//window.alert("This is our first Javascript Code!");
+
+/*
+    commenting
+*/
+
+// console.log("First console logging!")
+
+// window.console.log("Salaam from window!")
+
+// alert()
+// window
+// console.log()
+// script tag
+// comment
