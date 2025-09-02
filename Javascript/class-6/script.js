571b3c3d76d896895ab61608bc81545bf9f03501
Miftahul Islam <138649177+miftahulislam2023@users.noreply.github.com>
2025-09-02 23:47:58 +0600

    Add: class 7

---
+++ Javascript/class-6/script.js
@@ -0,0 +1,37 @@
+// let arr = [1, 'Two', 3.03, true, undefined, null]
+
+// console.log(arr)
+// console.log(arr[1])
+// console.log(arr.length)
+
+// arr[5] = 25
+// console.log(arr)
+// console.log(arr.length)
+
+// https://www.w3schools.com/js/js_arrays.asp
+/*
+An Array is an object type designed for storing data collections.
+
+Key characteristics of JavaScript arrays are:
+
+Elements: An array is a list of values, known as elements.
+
+Ordered: Array elements are ordered based on their index.
+
+Zero indexed: The first element is at index 0, the second at index 1, and so on.
+
+Dynamic size: Arrays can grow or shrink as elements are added or removed.
+
+Heterogeneous: Arrays can store elements of different data types (numbers, strings, objects and other arrays).
+*/
+
+//Array literal
+let nums1 = [1, 2, 3, 4]
+
+console.log(nums1)
+console.log(typeof nums1)
+
+//Array constructor
+let nums2 = new Array(1, 2, 3, 4);
+console.log(nums2)
+console.log(typeof nums2)
---
+++ Javascript/class-7/script.js
@@ -0,0 +1,31 @@
+//Array Nesting
+let x = [1, 2, 3]
+// let y = [4, 5, 6]
+// let z = [x, y, 7, 8, 9]
+// //Array flatening
+// console.log(z.flat())
+// let y = [x, 4, 5, 6]
+
+//Concatenation
+// let y = x.concat([4, 5, 6], [7, 8, 9, 10])
+// console.log(y)
+
+//***Spread operator-> ... (three dots)
+
+// let y = [x, 4, 5, 6]
+// y = [...x, 4, 5, 6]
+// console.log(y)
+// let y = [...x];
+// console.log(...x)
+// console.log(typeof y)
+
+//Array Static Methods
+let z = Array.isArray(x)
+
+//***Constructing/making array
+let str1 = 'MiftahCoding1'
+let str2 = 'MiftahCoding2'
+let y = Array.of(str1, str2)
+// let y = str.
+// console.log(str.__proto__)
+console.log(y)
