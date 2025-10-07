4baba0ba5f3e6796d733573a06b9846159f85cd7
Miftahul Islam <almiftahulislam@gmail.com>
2025-10-08 00:02:07 +0600

    Add: client js class 1

---
+++ Javascript/Client Side JS/class-1/script.js
@@ -0,0 +1,19 @@
+/*
+DOM - Document Object Model
+History
+- Static meaning no interaction
+- Dynamic website -> JavaScript
+
+Where is interactions happening?
+- HTML and CSS -> Document
+Document Object Model
+
+Global object
+- CLient side -> window
+*/
+
+// console.log(window.document)
+// console.log(window.document.body)
+
+window.console.log()
+console.log(window.document.body)
